d9a975d9a8adf1be835b1967a3bfed0f972e6275
huhn <sehe89@gmail.com>
2019-03-21 01:27:31 +0100

    add products to shopping cart

--- src/views/orders/components/OrderForm.vue
+++ src/views/orders/components/OrderForm.vue
@@ -21,6 +21,19 @@
         </el-row>
       </div>
     </el-form>
+
+
+      <h2>Order object</h2>
+      <pre>{{form}}</pre>
+
+      <h2>Product List</h2>
+      <u>
+        <li v-for="product in products" v-bind:key="product.data.id">
+          {{product.data.title}}, {{product.data.price}} {{product.data.currency}} 
+          <el-button @click="addToCart(product)" type="primary">Add</el-button>
+        </li>
+      </u>
+
   </div>
 </template>
 
@@ -41,10 +54,18 @@ export default {
   data() {
     return{
       loading: false,
+      products: [],
       form: {
-        name: ''
+        name: '',
+        cart: []
       }
     }
+  },
+  created() {
+    // fetch products from database
+    let products_string = localStorage.getItem('products') || "[]"
+    this.products = JSON.parse(products_string)
+    console.log("this.products", this.products)
   },
   methods: {
     onSubmit: async function() {
@@ -54,7 +75,6 @@ export default {
       let order = await createMAMChannel(this.form, 'order')
 
       // fetch orders from database
-
       let orders_string = localStorage.getItem('orders') || "[]"
       let orders = JSON.parse(orders_string)
 
@@ -69,6 +89,10 @@ export default {
     },
     onCancel() {
       this.$router.push('/orders')
+    },
+    addToCart(product) {
+      console.log("add product to card", product.data.title);
+      this.form.cart.push(product.root)
     }
   }
   
