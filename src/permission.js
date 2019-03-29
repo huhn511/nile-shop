bb7111c5b563ba35996b7741d489e9ab66986bdd
huhn <sehe89@gmail.com>
2019-03-29 02:08:59 +0100

    redirect to dashboard after login

--- src/permission.js
+++ src/permission.js
@@ -17,7 +17,7 @@ router.beforeEach((to, from, next) => {
     } else {
       if (store.getters.roles.length === 0) {
         store.dispatch('GetInfo').then(res => { // 拉取用户信息
-          next()
+          next({ path: '/dashboard' })
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
             Message.error(err || 'Verification failed, please login again')
