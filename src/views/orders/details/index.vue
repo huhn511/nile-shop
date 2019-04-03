fd048a4714582fd91d66a32098afe888864536c2
huhn <sehe89@gmail.com>
2019-04-04 01:23:31 +0200

    draw qr code for root

--- src/views/orders/details/index.vue
+++ src/views/orders/details/index.vue
@@ -2,7 +2,7 @@
     <div>
       <h2>QR - Code</h2>
       <div class="qrcode-container">
-      <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
+      <qrcode-vue :value="order.root" :size="size" level="H"></qrcode-vue>
       </div>
       <h2>Order data</h2>
       <pre>{{order}}</pre>
@@ -19,7 +19,6 @@ export default {
   data() {
     return {
       order: {},
-      value: 'https://example.com',
       size: 300
     }
   },
